f7b925cc58d80ffe97f3c8a5fdf04d01282fc5c9
luigi2001 <luigitanga@virgilio.it>
2021-09-07 20:35:07 +0200

    add database

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Comic;
 class ComicController extends Controller
 {
     public function comics() {
-        $data = config('comics');
+        $data = Comic::all();
         $data = ['comics' => $data];
         return view('main', $data);
     }
 
     public function details($id) {
-        $data = config('comics');
+        $data = Comic::all();
         $data = ['comic' => $data[$id]];
         return view('dettagli', $data);
     }
